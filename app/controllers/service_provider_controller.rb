3a07d1048c6256a93429e37cbd73a37acbef190c
DurgaPrasadRaju <durgaprasad.j@avowstech.com>
2023-10-11 23:46:58 +0800

    dashboard 5 issue fixed

--- app/controllers/service_provider_controller.rb
+++ app/controllers/service_provider_controller.rb
@@ -175,11 +175,15 @@ class ServiceProviderController < ApplicationController
 
         if(@certifydoctoroverallcount!=nil)
 	        @ceritifydoctorperyestotal=(@certifydoctorwithin24hours)
+	         if(@certifydoctoroverallcount.count>0)
 	        if(@certifydoctorbeyond24hours*100/@certifydoctoroverallcount<15)
 	          @ceritifydoctorperyestotal=@ceritifydoctorperyestotal+1
 	        else
 	          @ceritifydoctorperyestotal=@ceritifydoctorperyestotal+0
             end
+        else
+        	 @ceritifydoctorperyestotal=0
+        end
         else
     	    @ceritifydoctorperyestotal=0
     	    @ceritifydoctorpernototal=0
