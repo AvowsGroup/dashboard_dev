875a85ac0e6ea60a212f0dd38d57e3a83cf4564e
DurgaPrasadRaju <durgaprasad.j@avowstech.com>
2023-10-11 21:11:41 +0800

    commit changes

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,2 +1,4 @@
 class Laboratory < ApplicationRecord
+	 belongs_to :state, class_name: 'State', foreign_key: 'state_id'
+   belongs_to :status_schedule, class_name: 'StatusSchedule', foreign_key: 'id'
 end
--- app/models/xray_facility.rb
+++ app/models/xray_facility.rb
@@ -1,2 +1,5 @@
 class XrayFacility < ApplicationRecord
+	 belongs_to :state, class_name: 'State', foreign_key: 'state_id'
+	  belongs_to :status_schedule, class_name: 'StatusSchedule', foreign_key: 'id'
+    has_many :transactions, foreign_key: 'doctor_id'
 end
