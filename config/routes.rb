3ed1be5b7298eb0e9ebbc266a71f64b9baa5d21a
DurgaPrasadRaju <durgaprasad.j@avowstech.com>
2023-11-11 14:10:35 +0800

    all dashboard issues fixed

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'service_provider/index'
   get 'districts/:districtname' => 'second_dashboard#statevalues'
   get 'districtname/:districtvalue' => 'second_dashboard#districtvalues'
+  post 'refresh_dashboards' => 'refresh_dashboards#create'
+  get 'refresh_dashboards' => 'refresh_dashboards#get_interval'
   resources :first_dashboard ,only: [:index] do 
     collection do
       match 'excel_generate', to: 'first_dashboard#excel_generate', via: [:get, :post], defaults: { format: :xlsx }
